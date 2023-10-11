d786fcf65b1736864bf64ce5021dd9e086aa8f18
h-r-wells2 <hrwells10246@gmail.com>
2023-10-12 04:02:51 +0530

    updated existing sections

---
+++ script.js
@@ -0,0 +1,28 @@
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.8,
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const targetId = entry.target.id;
+        const correspondingLink = document.querySelector(`.right a[href="#${targetId}"]`);
+        if (correspondingLink) {
+          correspondingLink.style.color = '#ff6600';
+        }
+      } else {
+        const targetId = entry.target.id;
+        const correspondingLink = document.querySelector(`.right a[href="#${targetId}"]`);
+        if (correspondingLink) {
+          correspondingLink.style.color = 'white';
+        }
+      }
+    });
+  }, options);
+
+  const sections = document.querySelectorAll('section');
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
